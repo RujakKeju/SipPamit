15c31ab344f98650a0c4e866d5c7417fc7697dba
Amamiyakun02 <Amamiyakun02@gmail.com>
2023-08-06 10:35:05 +0700

    n

---
+++ resources/views/regex.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>regs</title>
+</head>
+<body>
+    <h1>register</h1>
+    <div class="main-form">
+        <form action="" method="post">
+            <div class="form-input">
+                
+            </div>
+            <div class="form-input">
+
+            </div>
+            <div class="form-input">
+
+            </div>
+            <div class="form-input">
+
+            </div>
+            <div class="form-input">
+
+            </div>
+            <div class="form-input">
+
+            </div>
+            <div class="form-input">
+
+            </div>
+            <div class="form-input">
+
+            </div>
+        </form>
+    </div>
+</body>
+</html>
