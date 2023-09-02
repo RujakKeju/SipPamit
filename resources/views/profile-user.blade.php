26d5d1e3ad8aaad3a1b02c7f9eced635516a5bd8
RujakKeju <msyabil158@gmail.com>
2023-09-02 21:16:50 +0800

    aku nambahkan profile.blade.php ama css nya profile.css, juga nambahkan table daftar investor di investnow

---
+++ resources/views/profile-user.blade.php
@@ -0,0 +1,425 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Gmail-like Interface</title>
+    <!-- Bootstrap CSS -->
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+    />
+    <link
+      href="https://fonts.googleapis.com/css?family=Muli:300,400,500,600,700,800,900&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+      integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="./public/assets/css/profile.css" />
+    <!-- Css Styles -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />
+    <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css" />
+    <link rel="stylesheet" href="css/themify-icons.css" type="text/css" />
+    <link rel="stylesheet" href="css/elegant-icons.css" type="text/css" />
+    <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css" />
+    <link rel="stylesheet" href="css/nice-select.css" type="text/css" />
+    <link rel="stylesheet" href="css/jquery-ui.min.css" type="text/css" />
+    <link rel="stylesheet" href="css/slicknav.min.css" type="text/css" />
+    <link rel="stylesheet" href="css/style.css" type="text/css" />
+  </head>
+  <body>
+    <!-- HEADER -->
+    <!-- Header Section Begin -->
+    <header class="header-section">
+      <div class="header-top">
+        <div class="container">
+          <div class="ht-left">
+            <div class="mail-service">
+              <i class="fa fa-envelope"></i>
+              hello.colorlib@gmail.com
+            </div>
+            <div class="phone-service">
+              <i class="fa fa-phone"></i>
+              +65 11.188.888
+            </div>
+          </div>
+          <div class="ht-right">
+            <a href="#" class="login-panel"><i class="fa fa-user"></i>Login</a>
+            <div class="lan-selector">
+              <select
+                class="language_drop"
+                name="countries"
+                id="countries"
+                style="width: 300px"
+              >
+                <option
+                  value="yt"
+                  data-image="img/flag-1.jpg"
+                  data-imagecss="flag yt"
+                  data-title="English"
+                >
+                  English
+                </option>
+                <option
+                  value="yu"
+                  data-image="img/flag-2.jpg"
+                  data-imagecss="flag yu"
+                  data-title="Bangladesh"
+                >
+                  German
+                </option>
+              </select>
+            </div>
+            <div class="top-social">
+              <a href="#"><i class="ti-facebook"></i></a>
+              <a href="#"><i class="ti-twitter-alt"></i></a>
+              <a href="#"><i class="ti-linkedin"></i></a>
+              <a href="#"><i class="ti-pinterest"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="inner-header">
+          <div class="row">
+            <div class="col-lg-2 col-md-2">
+              <div class="logo">
+                <a href="./index.html">
+                  <img src="img/logo.png" alt="" />
+                </a>
+              </div>
+            </div>
+            <div class="col-lg-7 col-md-7">
+              <div class="advanced-search">
+                <button type="button" class="category-btn">
+                  All Categories
+                </button>
+                <form action="#" class="input-group">
+                  <input type="text" placeholder="What do you need?" />
+                  <button type="button"><i class="ti-search"></i></button>
+                </form>
+              </div>
+            </div>
+            <div class="col-lg-3 text-right col-md-3">
+              <ul class="nav-right">
+                <li class="heart-icon">
+                  <a href="#">
+                    <i class="icon_heart_alt"></i>
+                    <span>1</span>
+                  </a>
+                </li>
+                <li class="cart-icon">
+                  <a href="#">
+                    <i class="icon_bag_alt"></i>
+                    <span>3</span>
+                  </a>
+                  <div class="cart-hover">
+                    <div class="select-items">
+                      <table>
+                        <tbody>
+                          <tr>
+                            <td class="si-pic">
+                              <img src="img/select-product-1.jpg" alt="" />
+                            </td>
+                            <td class="si-text">
+                              <div class="product-selected">
+                                <p>$60.00 x 1</p>
+                                <h6>Kabino Bedside Table</h6>
+                              </div>
+                            </td>
+                            <td class="si-close">
+                              <i class="ti-close"></i>
+                            </td>
+                          </tr>
+                          <tr>
+                            <td class="si-pic">
+                              <img src="img/select-product-2.jpg" alt="" />
+                            </td>
+                            <td class="si-text">
+                              <div class="product-selected">
+                                <p>$60.00 x 1</p>
+                                <h6>Kabino Bedside Table</h6>
+                              </div>
+                            </td>
+                            <td class="si-close">
+                              <i class="ti-close"></i>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                    <div class="select-total">
+                      <span>total:</span>
+                      <h5>$120.00</h5>
+                    </div>
+                    <div class="select-button">
+                      <a href="#" class="primary-btn view-card">VIEW CARD</a>
+                      <a href="#" class="primary-btn checkout-btn">CHECK OUT</a>
+                    </div>
+                  </div>
+                </li>
+                <li class="cart-price">$150.00</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="nav-item">
+        <div class="container">
+          <div class="nav-depart">
+            <div class="depart-btn">
+              <i class="ti-menu"></i>
+              <span>All departments</span>
+              <ul class="depart-hover">
+                <li class="active"><a href="#">Peralatan</a></li>
+                <li><a href="#">Pakan Ternak</a></li>
+                <li><a href="#">Ayam Potong</a></li>
+                <li><a href="#">Obat</a></li>
+              </ul>
+            </div>
+          </div>
+          <nav class="nav-menu mobile-menu">
+            <ul>
+              <li><a href="./index.html">Home</a></li>
+              <li><a href="./shop.html">Shop</a></li>
+              <li><a href="invest.html">Investasi</a></li>
+              <li>
+                <a href="#">Pages</a>
+                <ul class="dropdown">
+                  <li><a href="./shopping-cart.html">Shopping Cart</a></li>
+                  <li><a href="./register.html">Register</a></li>
+                  <li><a href="./login.html">Login</a></li>
+                </ul>
+              </li>
+            </ul>
+          </nav>
+          <div id="mobile-menu-wrap"></div>
+        </div>
+      </div>
+    </header>
+    <!-- Header End -->
+
+    <div class="container-fluid mt-4">
+      <div class="row">
+        <nav id="sidebar">
+          <div class="list-group">
+            <a href="#account" class="list-group-item list-group-item-action"
+              >Akun Saya</a
+            >
+            <a href="#Investasi" class="list-group-item list-group-item-action"
+              >Investasi</a
+            >
+            <a href="#sent" class="list-group-item list-group-item-action"
+              >Sent</a
+            >
+            <a href="#drafts" class="list-group-item list-group-item-action"
+              >Drafts</a
+            >
+          </div>
+        </nav>
+        <!-- Mulainya -->
+        <main id="content">
+          <div id="account" style="display: none">
+            <h2>Akun Saya</h2>
+            <form>
+              <div class="form-row">
+                <div class="form-group col-md-6">
+                  <label for="name">Nama</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="name"
+                    value="John Doe"
+                  />
+                </div>
+                <div class="form-group col-md-6">
+                  <label for="address">Alamat</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="address"
+                    value="123 Main St"
+                  />
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="email">Email</label>
+                <input
+                  type="email"
+                  class="form-control"
+                  id="email"
+                  value="johndoe@example.com"
+                />
+              </div>
+              <div class="form-group">
+                <label for="phone">No. HP</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="phone"
+                  value="123-456-7890"
+                />
+              </div>
+              <div class="form-group">
+                <label>Jenis Kelamin</label>
+                <div class="form-check">
+                  <input
+                    class="form-check-input"
+                    type="radio"
+                    name="gender"
+                    id="genderMale"
+                    value="male"
+                    checked
+                  />
+                  <label class="form-check-label" for="genderMale"
+                    >Laki-laki</label
+                  >
+                </div>
+                <div class="form-check">
+                  <input
+                    class="form-check-input"
+                    type="radio"
+                    name="gender"
+                    id="genderFemale"
+                    value="female"
+                  />
+                  <label class="form-check-label" for="genderFemale"
+                    >Perempuan</label
+                  >
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="birthdate">Tanggal Lahir</label>
+                <input
+                  type="date"
+                  class="form-control"
+                  id="birthdate"
+                  value="1990-01-01"
+                />
+              </div>
+              <div class="photo">
+                <div class="form-group">
+                  <label for="profilePhoto">Foto Profil</label>
+                  <input
+                    type="file"
+                    class="form-control-file"
+                    id="profilePhoto"
+                    style="display: none"
+                  />
+                  <label for="profilePhoto" class="custom-file-upload">
+                    Pilih File Foto Profil
+                  </label>
+                </div>
+                <div class="form-group text-center">
+                  <img
+                    id="previewProfileImage"
+                    src="default-profile.jpg"
+                    alt="Preview Foto Profil"
+                    width="200"
+                    height="200"
+                    style="cursor: pointer"
+                  />
+                </div>
+              </div>
+
+              <button type="submit" class="btn btn-primary">Simpan</button>
+            </form>
+          </div>
+          <div id="Investasi" style="display: none">
+            <h2>Daftar Investasi</h2>
+            <table class="table">
+              <thead>
+                <tr>
+                  <th>Peternakan</th>
+                  <th>Kontribusi</th>
+                  <th>Tanggal Mulai</th>
+                  <th>Aksi</th>
+                </tr>
+              </thead>
+              <tbody>
+                <!-- Data investasi akan ditambahkan di sini -->
+                <tr>
+                  <td>Nama Peternakan 1</td>
+                  <td>Rp 1,000,000</td>
+                  <td>01-01-2023</td>
+                  <td><a href="#">Lihat</a></td>
+                </tr>
+                <tr>
+                  <td>Nama Peternakan 2</td>
+                  <td>Rp 500,000</td>
+                  <td>02-01-2023</td>
+                  <td><a href="invest-now.html">Lihat</a></td>
+                </tr>
+                <!-- Tambahkan baris lain sesuai dengan data investasi -->
+              </tbody>
+            </table>
+          </div>
+
+          <div id="sent" style="display: none">
+            <!-- Konten sent di sini -->
+          </div>
+          <div id="drafts" style="display: none">
+            <!-- Konten drafts di sini -->
+          </div>
+        </main>
+      </div>
+    </div>
+
+    <!-- Bootstrap JS and jQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script>
+      const sidebar = document.getElementById("sidebar");
+      const content = document.getElementById("content");
+
+      function showSection(sectionId) {
+        const sections = content.children;
+        for (let i = 0; i < sections.length; i++) {
+          sections[i].style.display = "none";
+        }
+        document.getElementById(sectionId).style.display = "block";
+      }
+
+      sidebar.addEventListener("click", (event) => {
+        if (event.target.tagName === "A") {
+          showSection(event.target.getAttribute("href").substr(1));
+        }
+      });
+
+      showSection("account"); // Tampilkan Akun Saya secara default
+    </script>
+    <script>
+      // Menampilkan dialog pemilihan file saat gambar di klik
+      document
+        .getElementById("previewProfileImage")
+        .addEventListener("click", function () {
+          document.getElementById("profilePhoto").click();
+        });
+
+      // Menampilkan gambar yang dipilih oleh pengguna
+      document
+        .getElementById("profilePhoto")
+        .addEventListener("change", function () {
+          const fileInput = this;
+          const previewProfileImage = document.getElementById(
+            "previewProfileImage"
+          );
+
+          if (fileInput.files && fileInput.files[0]) {
+            const reader = new FileReader();
+
+            reader.onload = function (e) {
+              previewProfileImage.src = e.target.result;
+            };
+
+            reader.readAsDataURL(fileInput.files[0]);
+          }
+        });
+    </script>
+  </body>
+</html>
